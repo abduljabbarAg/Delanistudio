e37ef39eed5eba7310b2f461ee6cd17cca49b7bd
AJ <abdijabarboxer@gmail.com>
2020-07-26 01:51:54 +0300

    added guide comments to script.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
+//    what we do section
+
 $('.main').click(function () {
-  $(this).children('.hide').slideToggle(1000);
-  $(this).children('.show').toggle(2000);
+    // hides the paragraph with the description on click of the image in the what we do section after 1 second
+    $(this).children('.hide').slideToggle(1000);
+    // shows the paragraph with the description on click of the image in the what we do section after 2 seconds
+    $(this).children('.show').toggle(2000);
   });
+  
