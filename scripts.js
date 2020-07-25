7604cc3635f4c524e737a86659ce343f87702d7f
AJ <abdijabarboxer@gmail.com>
2020-07-26 02:43:40 +0300

    added alert function to the contacts submit button

--- scripts.js
+++ scripts.js
@@ -14,4 +14,11 @@ $('.main').click(function () {
 $(".card").hover(function () {
     $(this).children(".card-1").fadeToggle(1000, "linear");
   });
+
   
+
+$("button").click(function (getDetails) {
+    var client = document.getElementById('nameDetail').value;
+    alert(' Dear ' + client + ' Thank you for reaching out. We will get back to you within 24 hours max.' + ' Delani Studio Team ');
+    getDetails.preventDefault();
+  });
